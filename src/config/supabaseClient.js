68a0c6aa295b4719ca0f5316d2c371acab4a46de
Thibaut Izard <thibaut.izard@gmail.com>
2023-01-29 00:07:47 +0100

    Corrections pour deploy 4/n

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_ANON_KEY;
+const supabaseUrl = "https://mzupudupmctanfznecnz.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dXB1ZHVwbWN0YW5mem5lY256Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzIyNTk0MzEsImV4cCI6MTk4NzgzNTQzMX0.Y5JeH5ymjzt68sSq9Iz6iKkGa8j9Mo70Cd8WhoLiob0";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;

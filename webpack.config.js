7bfb0f8a92b5532870f1c16b4b223e6848fe92ad
Thibaut Izard <thibaut.izard@gmail.com>
2023-01-29 10:54:40 +0100

    Correctif image

--- webpack.config.js
+++ webpack.config.js
@@ -29,14 +29,6 @@ module.exports = {
           },
         ],
       },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
     ],
   },
   resolve: {
